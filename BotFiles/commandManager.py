0d06a0c71b7b1b06eb56f56ad7cd44f039a9375a
jjuicecup <alexgiberson7@gmail.com>
2020-06-25 14:32:14 -0400

    Added command functionality from discord

--- BotFiles/commandManager.py
+++ BotFiles/commandManager.py
@@ -1,4 +1,3 @@
 
-class commandManager:
-    pass
+
 
--- BotFiles/main.py
+++ BotFiles/main.py
@@ -1,8 +1,23 @@
 import os
 import discord
+from serverManager import ListenServerCommands
 from dotenv import load_dotenv
-from executableManager import startServer
 
-process = None
-while True:
-    startServer(process)
+TOKEN = '...'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    messageContent = message.content
+    response = ListenServerCommands(messageContent)
+    await message.channel.send(response)
+
+client.run(TOKEN)
